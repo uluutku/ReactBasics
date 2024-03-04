82767c8601a190a2cd335aebafee7ddc9e7c3c8f
ayk62 <kirbacaycan@gmail.com>
2024-03-04 22:34:14 +0300

    fixed biseler

--- 12-ColorPickerApp/color-picker/src/ColorPicker.jsx
+++ 12-ColorPickerApp/color-picker/src/ColorPicker.jsx
@@ -13,8 +13,9 @@ function ColorPicker(){
         <>
         <div className="color-picker-container">
             <h1>Color Picker App</h1>
+            <p className="davsan">Selected Color: {color}</p>
             <div className="color-display" style={{backgroundColor: color}}>
-            <p>Selected Color: {color}</p>
+           
             </div>
             <label>Select a Color:</label>
             <input type="color" value={color} onChange={handleColorChange}></input>
